98d1f105a3c424b6f4fb65610791cdc329613af7
Nate Tangsurat <ntangsurat@gogoair.com>
2015-10-09 23:41:37 -0500

    Buttloads of debugging

--- pyasgard/pyasgard.py
+++ pyasgard/pyasgard.py
@@ -1,17 +1,20 @@
 __author__ = "Sijis Aviles <saviles@gogoair.com>"
 __version__ = "1.0"
 
-import re
 import base64
-
-try:
-    import simplejson as json
-except:
-    import json
+import logging
+import re
+from pprint import pformat
 
 import requests
 
 from endpoints import mapping_table as mapping_table
+
+# try:
+#     import simplejson as json
+# except ImportError:
+#     import json
+
 
 def clean_kwargs(kwargs):
     """Format the kwargs to conform to API"""
@@ -19,6 +22,7 @@ def clean_kwargs(kwargs):
     for key, value in kwargs.iteritems():
         if hasattr(value, '__iter__'):
             kwargs[key] = ','.join(map(str, value))
+
 
 def decrypt_hash(string):
     string = base64.b64decode(string)
@@ -36,7 +40,7 @@ class AsgardError(Exception):
 
 class AuthenticationError(AsgardError):
     def __init__(self, msg):
-        self.msg = msg 
+        self.msg = msg
 
     def __str__(self):
         return repr(self.msg)
@@ -45,9 +49,14 @@ class AuthenticationError(AsgardError):
 class Asgard(object):
     """ Python API Wrapper for Asgard"""
 
-    def __init__(self, url, username=None,
-                 password=None, headers=None,
-                 client_args={}, api_version=1, ec2_region='us-east-1'):
+    def __init__(self,
+                 url,
+                 username=None,
+                 password=None,
+                 headers=None,
+                 client_args={},
+                 api_version=1,
+                 ec2_region='us-east-1'):
         """
         Instantiates an instance of Asgard. Takes optional parameters for
         HTTP Basic Authentication
@@ -65,6 +74,7 @@ class Asgard(object):
             or a common one is to disable SSL certficate validation
             {"disable_ssl_certificate_validation": True}
         """
+        logging.debug(pformat(locals()))
         self.data = None
         self.url = '{}/{}'.format(url.rstrip('/'), ec2_region)
         self.username = username
@@ -99,28 +109,34 @@ class Asgard(object):
         TODO:
             Should probably url-encode GET query parameters on replacement
         """
-        def call(self, **kwargs):
-            """ """
-            api_map = self.mapping_table[api_call]
-            path = api_map['path']
 
+        def call(self, **kwargs):
+            """Request to API based on method and endpoint mapping."""
+            logging.debug(pformat(locals()))
+
+            api_map = self.mapping_table[api_call]
+            logging.debug(pformat(api_map))
+
+            path = api_map['path']
             method = api_map['method']
             status = api_map['status']
             valid_params = api_map.get('valid_params', ())
+            logging.debug('valid_params=%s', valid_params)
             # Body can be passed from data or in args
             body = kwargs.pop('data', None) or self.data
 
             # Substitute mustache '{{}}' placeholders with data from keywords
-            url = re.sub(
-                '\{\{(?P<m>[a-zA-Z_]+)\}\}',
-                # Optional pagination parameters will default to blank
-                #lambda m: "%s" % kwargs.pop(m.group(1), ''),
-                lambda m: '{}'.format(kwargs.pop(m.group(1), '')),
-                '{}{}'.format(self.url, path)
-            )
+            # Optional pagination parameters will default to blank
+            # regex_pattern = '\{\{(?P<m>[a-zA-Z_]+)\}\}'
+            regex_pattern = '{{(?P<m>[a-zA-Z_]+)}}'
+            url = re.sub(regex_pattern,
+                         lambda m: '{}'.format(kwargs.pop(m.group(1), '')),
+                         '{}{}'.format(self.url, path))
+            logging.debug(url)
 
             # Validate remaining kwargs against valid_params and add
             # params url encoded to url variable.
+            logging.debug(kwargs)
             for kw in kwargs:
                 if kw not in valid_params:
                     raise TypeError("%s() got an unexpected keyword argument "
@@ -135,18 +151,27 @@ class Asgard(object):
             }
 
             if self.username and self.password:
-                auth = { 'auth': (self.username, decrypt_hash(self.password)) }
+                auth = {'auth': (self.username, decrypt_hash(self.password))}
                 url_params.update(auth)
 
+            logging.debug(
+                'getattr(%s, %s)(%s)', requests, method.lower(), pformat({
+                    key: value
+                    for key, value in url_params.items() if key != 'auth'
+                }))
+
             response = getattr(requests, method.lower())(**url_params)
+            logging.debug(response)
+
             return self._response_handler(response, status)
 
+        logging.debug('Looking up %s', api_call)
         # Missing method is also not defined in our mapping table
         if api_call not in self.mapping_table:
             raise AttributeError('Method "%s" Does Not Exist' % api_call)
 
         # Execute dynamic method and pass in keyword args as data to API call
-        return call.__get__(self)
+        return call.__get__(self)  # pylint: disable=E1101
 
     @staticmethod
     def _response_handler(response, status):
@@ -156,13 +181,21 @@ class Asgard(object):
         If the response status is different from status defined in the
         mapping table, then we assume an error and raise proper exception
         """
+        logging.debug(pformat(locals()))
+
         # Just in case
         if response is None:
-            raise AsgardError('Response Not Found')
+            message = 'Response Not Found'
+            logging.error(message)
+            raise AsgardError(message)
 
         if response.status_code != status:
-            raise AsgardError(response.reason, response.status_code)
+            error = AsgardError(response.reason, response.status_code)
+            logging.fatal(error)
+            raise error
 
         # Deserialize json content if content exist. In some cases Asgard
         # returns ' ' strings. Also return false non strings (0, [], (), {})
-        return response.json()
+        response_json = response.json()
+        logging.debug(pformat(response_json))
+        return response_json
