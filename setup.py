c4cbc2c670a5a7269e1062494796f1048d0b84c2
Nate Tangsurat <ntangsurat@gogoair.com>
2015-10-29 09:59:59 -0500

    v1.1 includes new requirement BeautifulSoup4

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-setup(
-    name='pyasgard',
-    version = '1.0',
-    description = 'Python library for working with the Asgard api.',
-    long_description = open('README.md').read(),
-    author = 'Sijis Aviles',
-    author_email = 'saviles@gogoair.com',
-    packages = find_packages(),
-    install_requires = ['requests'],
-    keywords = "asgard api python netflixoss",
-    url = 'https://github.com/gogoit/pyasgard',
-    download_url = 'https://github.com/gogoit/pyasgard',
-    platforms = ['OS Independent'],
-    classifiers = [
-        'Development Status :: 1 - Beta',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 2.7',
-        'Operating System :: OS Independent',
-    ],
-)
-
+setup(name='pyasgard',
+      version='1.1',
+      description='Python library for working with the Asgard api.',
+      long_description=open('README.md').read(),
+      author='Sijis Aviles',
+      author_email='saviles@gogoair.com',
+      packages=find_packages(),
+      install_requires=['beautifulsoup4',
+                        'requests', ],
+      keywords="asgard api python netflixoss",
+      url='https://github.com/gogoit/pyasgard',
+      download_url='https://github.com/gogoit/pyasgard',
+      platforms=['OS Independent'],
+      classifiers=[
+          'Development Status :: 1 - Beta',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 2.7',
+          'Operating System :: OS Independent',
+      ], )
